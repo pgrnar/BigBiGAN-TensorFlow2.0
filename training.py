8db4dc42df7d95cc38b06c2b9467402cb54d6aa9
Paulina Grnarova <pgrnarova@gmail.com>
2020-06-03 13:32:05 +0200

    correct order of minmax and maxmin for the evaluation of dg

--- training.py
+++ training.py
@@ -164,10 +164,10 @@ def train_epoch(train_step_fn_d_const, train_step_fn_g_const, forward_pass_fn_d_
             with tf.GradientTape() as tape:
                 g_e_loss, d_loss_g_const = forward_pass_fn_g_const(image, label, gen, model_copies[disc_f], model_copies[disc_h], model_copies[disc_j], model_en, batch_size, cont_dim, config)
                 grad_for_g = [-g for g in tape.gradient(d_loss_g_const, g_vars)]
+            metric_loss_dg.update_state(d_loss_g_const - d_loss_d_const) # first evaluate to ensure dg>=0, then update
             if not config.do_eval:
                 dg_optimizer_d.apply_gradients(zip(grad_for_d, d_vars))
                 dg_optimizer_g.apply_gradients(zip(grad_for_g, g_vars))
-            metric_loss_dg.update_state(d_loss_d_const - d_loss_g_const)
         else:
             train_step_fn_d_const(image, label, gen, disc_f, disc_h, disc_j, model_en, disc_optimizer, gen_en_optimizer, metric_loss_disc, metric_loss_gen_en, batch_size, cont_dim, config)
 
